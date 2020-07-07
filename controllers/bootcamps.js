f2766ce33811936056bc5ae80c436e7eef1ebea9
Kevin O Keeffe <kevokeeffe@gmail.com>
2020-07-07 19:35:53 +0100

    Select & Sorting added to api query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,21 +9,50 @@ const geocoder = require('../utils/geocoder');
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
 
-  let queryStr = JSON.stringify(req.query);
+  // Copy req.query
+  const reqQuery = { ...req.query };
 
+  // Fields to exclude
+  const removeFields = ['select', 'sort'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, etc)
   queryStr = queryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
 
+  // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    console.log(fields);
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Executing query
   const bootcamps = await query;
 
+  // Validating request
   if (bootcamps.length < 1 || bootcamps == undefined) {
     return next(
       new ErrorResponse(
-        `There are no bootcamps registered on the database.`,
+        `I'm sorry but your query has returned no results.`,
         404
       )
     );
