841fbefc8e0455c84212c512770ba7d3815ceedb
Kevin O Keeffe <kevokeeffe@gmail.com>
2020-07-09 20:30:35 +0100

    Added photo upload for bootcamp - used express-fileupload package.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,6 @@
+// This is a core node.js module for dealing with file paths
+const path = require('path');
+
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
@@ -170,5 +173,52 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     success: true,
     count: bootcamps.length,
     data: bootcamps,
+  });
+});
+
+// @desc    Upload photo for bootcamp
+// @route   PUT /api/v1/bootcamps/:id/photo
+// @access  Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse(`Please upload a file`, 400));
+  }
+  const file = req.files.file;
+
+  // Make sure the image is a photo
+  if (!file.mimetype.startsWith('image')) {
+    return next(new ErrorResponse(`Please upload an image file`, 415));
+  }
+
+  // Check filesize
+  if (file.size > process.env.MAX_FILE_UPLOAD) {
+    return next(
+      new ErrorResponse(
+        `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
+        413
+      )
+    );
+  }
+
+  // Create custom filename
+  file.name = `${path.parse(file.name).name}_photo_${bootcamp._id}_${Math.round(
+    new Date().getTime()
+  )}_${path.parse(file.name).ext}`;
+
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+    if (err) {
+      console.log(err);
+      return next(new ErrorResponse(`Problem with file upload`, 501));
+    }
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+    res.status(200).json({ success: true, data: file.name });
   });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,17 +6,20 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
 
 // Include other resourse routers
-const courseRouter = require('./courses')
+const courseRouter = require('./courses');
 
 const router = express.Router();
 
 // Re-route into other resourse routers
-router.use('/:bootcampId/courses', courseRouter)
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router.route('/:id/photo').put(bootcampPhotoUpload);
 
 router.route('/').get(getBootcamps).post(createBootcamp);
 
