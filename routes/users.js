dcf90e744756f2ced7c030bc1e576549047b3faf
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-07-24 22:23:23 +0100

    Admin Users CRUD

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+// Controllers
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/users');
+
+// Models
+const User = require('../models/User');
+
+// Nest routers
+const router = express.Router({ mergeParams: true });
+
+// Middleware
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorize } = require('../middleware/auth');
+
+
+// Anything below this router.use(), uses contained middleware.
+router.use(protect);
+router.use(authorize('admin'));
+
+router.route('/').get(advancedResults(User), getUsers).post(createUser);
+
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
